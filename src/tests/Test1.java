c0c27eaaa0fc1ebf8b018c5690d7ea3d0af57fbd
mgadhiya01 <33448436+mgadhiya01@users.noreply.github.com>
2017-12-06 10:37:05 +0530

    Add files via upload

---
+++ src/tests/Test1.java
@@ -0,0 +1,34 @@
+package tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	public static WebDriver driver;
+	@BeforeSuite
+	public static void setup(){
+		System.setProperty("webdriver.gecko.driver", "/var/lib/geckodriver");
+		System.setProperty("webdriver.firefox.bin", "/usr/bin/firefox");
+		driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		driver.get("http://ebsdevops03.compute-a436090.oraclecloud.internal:8000/OA_HTML/AppsLogin");
+		
+	}
+	@Test
+	public static void login(){
+		driver.findElement(By.xpath("//form[@id='login']//input[@name='usernameField']")).sendKeys("MBHARMAL");
+		driver.findElement(By.xpath("//form[@id='login']//input[@name='passwordField']")).sendKeys("Welcome123");
+		driver.findElement(By.xpath("//*[@id='ButtonBox']/button")).click();
+		String message=driver.getTitle();
+		System.out.println("Page title "+message);
+	}
+	
+	
+}
