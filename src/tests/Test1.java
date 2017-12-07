901ef563f5c82a639aaa228c185ba9c73ac9693f
mgadhiya01 <33448436+mgadhiya01@users.noreply.github.com>
2017-12-07 10:59:00 +0530

    Add files via upload

--- src/tests/Test1.java
+++ src/tests/Test1.java
@@ -4,8 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -16,7 +16,7 @@ public class Test1 {
 		System.setProperty("webdriver.gecko.driver", "/var/lib/geckodriver");
 		System.setProperty("webdriver.firefox.bin", "/usr/bin/firefox");
 		driver=new FirefoxDriver();
-		//driver.manage().window().maximize();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
 		driver.get("http://ebsdevops03.compute-a436090.oraclecloud.internal:8000/OA_HTML/AppsLogin");
 		
@@ -24,9 +24,9 @@ public class Test1 {
 	@Test
 	public static void login(){
 		driver.findElement(By.xpath("//form[@id='login']//input[@name='usernameField']")).sendKeys("MBHARMAL");
-		driver.findElement(By.xpath("//form[@id='login']//input[@name='passwordField']")).sendKeys("Nov@2017");
-		driver.findElement(By.xpath("//div[@class='control_box center']/button[@class='OraButton left']")).click();
-		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//form[@id='login']//input[@name='passwordField']")).sendKeys("Welcome123");
+		driver.findElement(By.xpath("//*[@id='ButtonBox']/button")).click();
+		
 		try {
 			Thread.sleep(60000);
 		} catch (InterruptedException e) {
@@ -37,7 +37,30 @@ public class Test1 {
 		String message=driver.getTitle();
 		System.out.println("Page title "+message);
 	}
+	@Test
+	public static void searchProduct(){
+		
+		driver.findElement(By.xpath("//form[@name='DefaultFormName']//table[@class='xhq']//a[@class='OraUniversalNavigator']")).click();
+		driver.findElement(By.xpath("//form[@name='DefaultFormName']//table[@class='xhq']//a[@class='OraUniversalNavigator']//input[@id='navSearch']")).sendKeys("iprocurement");
+		try {
+			Thread.sleep(60000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//form[@name='DefaultFormName']//table[@class='xhq']/tr[@class='x8e']//div[@id='navContainer0']//div[@class='listContainer']//a[@class='entryLink']")).click();
+		try {
+			Thread.sleep(60000);
+			System.out.println("clicked on procurement ");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.findElement(By.xpath("//form[@name='DefaultFormName']//table[@id='SearchTextInput__xc_']//input[@id='SearchTextInput']")).sendKeys("blank");
+		
+		
+		
 	}
 	
-	
-
+}
